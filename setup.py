b1b9b6c9847bc747c6463745f262c67c6d86f11e
dapolak <deirdree.polak@gmail.com>
2019-08-26 15:47:00 +0100

    package struvture

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="slugdetection",
+    version="0.0.1",
+    author="dapolak",
+    author_email="deirdree.polak@gmail.com",
+    description="Slugging Detection package using feature engineering "
+                "and supervised classification for offshore oil wells.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msc-acse/acse-9-project-plan-dapolak",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independant",
+    ],
+)
