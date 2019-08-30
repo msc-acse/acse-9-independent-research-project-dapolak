8889ec12eb796a365b68ef620bf522a21d488f12
dapolak <38186338+dapolak@users.noreply.github.com>
2019-08-30 09:20:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slugdetection",
-    version="0.0.1",
+    version="1.0.0,
     author="dapolak",
     author_email="deirdree.polak@gmail.com",
     description="Slugging Detection package using feature engineering "
